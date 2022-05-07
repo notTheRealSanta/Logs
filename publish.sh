9c4024f938dedca8344b598b9fc8ae743c505c5d
nottherealsanta <ucsantosh@gmail.com>
2022-05-07 11:35:59 +0530

    Added custom.css

--- publish.sh
+++ publish.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+rm _build/static/css/custom.css
+cp custom.css _build/static/css/custom.css
 git commit -am "Save uncommited changes (WIP)"
 git branch --delete --force gh-pages
 git checkout --orphan gh-pages
 git add -f _build
 git commit -m "Rebuild GitHub pages"
-git filter-branch -f --prune-empty --subdirectory-filter _build && git push -f origin gh-pages && git checkout -
+git filter-branch -f --prune-empty --subdirectory-filter _build && git push -f origin gh-pages
