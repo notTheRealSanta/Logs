bd018b2ee7da578466d9fb36f79b0b8052e97b18
nottherealsanta <ucsantosh@gmail.com>
2022-05-19 09:43:11 +0530

    Thu 19 May 2022 09:43:11 IST

--- publish.sh
+++ publish.sh
@@ -7,10 +7,5 @@ cp cp/custom.css _build/static/css/custom.css
 
 cp cp/Flammarion.png _build/static/img/logo.png
 
-git commit -am "Save uncommited changes (WIP)"
-git branch --delete --force gh-pages
-git checkout --orphan gh-pages
-git add -f _build
-git commit -m "Rebuild GitHub pages"
-git push -f origin gh-pages
-git checkout main
+git commit -am "`date`"
+git subtree push --prefix _build origin gh-pages
