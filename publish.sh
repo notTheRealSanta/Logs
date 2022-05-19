1e2fac733de357c922a5dfb1a8cf85f5ad5bf7f5
nottherealsanta <ucsantosh@gmail.com>
2022-05-19 09:30:46 +0530

    Save uncommited changes (WIP)

--- publish.sh
+++ publish.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
 osascript -e 'quit app "Logseq"'
+
+rm _build/static/css/custom.css
+cp cp/custom.css _build/static/css/custom.css
+
+cp cp/Flammarion.png _build/static/img/logo.png
+
+git commit -am "Save uncommited changes (WIP)"
+git branch --delete --force gh-pages
+git checkout --orphan gh-pages
+git add -f _build
+git commit -m "Rebuild GitHub pages"
+git push -f origin gh-pages
+git checkout main
