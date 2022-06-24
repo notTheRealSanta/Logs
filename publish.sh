92b14099fd11c8f88ebfdf8aee0892c763112e19
nottherealsanta <ucsantosh@gmail.com>
2022-06-24 15:08:30 +0530

    update 29th May

--- publish.sh
+++ publish.sh
@@ -7,7 +7,6 @@ cp cp/custom.css _build/static/css/custom.css
 
 cp cp/Flammarion.png _build/static/img/logo.png
 
-git add .
 git commit -m "`date`"
 git subtree add --prefix=_build origin gh-pages
 git subtree push --prefix=_build origin gh-pages
